73a99c08340f9ce2cb4ce705a4b6fe6a7780cad3
Manuel A. Vázquez <manuavazquez@gmail.com>
2021-05-27 17:11:33 +0200

    first actual commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Source code to reproduce the results in the paper
+
+> An interpretable machine learning method for the detection of schizophrenia using EEG signals. Manuel A. Vázquez, Arash Maghsoudi, and Inés P. Mariño.
+
+*Raw* data can be downloaded [here](https://repod.icm.edu.pl/dataset.xhtml?persistentId=doi%3A10.18150%2Frepod.0107441&fbclid=IwAR3xp2T6VkBSWK_T9hOKp6cIEq_ac1I4dzSnxVLPbowDyd37hgAZUm59QdQ). A related paper is freely available at [PLOS](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0188629)
---
+++ make_conda_environment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MANAGER="mamba"
+
+COLOR="\033[40m\033[32m"
+UNCOLOR="\033[0m"
+
+NAME=schizo_eeg
+
+# only required if "anaconda" is not in the path
+source $HOME/anaconda3/etc/profile.d/conda.sh
+# source $HOME/gts/anaconda3/etc/profile.d/conda.sh # arvak, hidra*...
+
+$MANAGER create --yes -n $NAME fastbook cudatoolkit=10.1 jupyterlab bokeh -c defaults -c conda-forge -c fastchan -c pytorch
+
+conda activate $NAME
+
+# for visualizing decision trees
+pip install dtreeviz
+
+echo -e new environment is \"$COLOR$NAME$UNCOLOR\"
